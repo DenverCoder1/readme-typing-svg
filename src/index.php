b46334f0886414202e906a48c4379fca8d3e7b12
Jonah Lawrence <jonah@freshidea.com>
2021-04-20 04:29:33 +0300

    Index for rendering SVG

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php declare (strict_types = 1);
+
+require_once "models/RendererModel.php";
+require_once "views/RendererView.php";
+
+// set content type
+header("Content-type: image/svg+xml");
+
+$model = new RendererModel();
+$view = new RendererView($model);
+
+// render SVG
+$view->output();
