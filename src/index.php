873451dab5cc8ec936907c46906d79759cd4439b
Jonah Lawrence <jonah@freshidea.com>
2021-04-20 07:35:35 +0300

    Use error view if renderer fails

--- src/index.php
+++ src/index.php
@@ -2,6 +2,8 @@
 
 require_once "models/RendererModel.php";
 require_once "views/RendererView.php";
+require_once "src/models/ErrorModel.php";
+require_once "src/views/ErrorView.php";
 
 // set content type
 header("Content-type: image/svg+xml");
@@ -9,13 +11,14 @@ header("Content-type: image/svg+xml");
 try {
     // create renderer model
     $model = new RendererModel("templates/main.php", $_REQUEST);
+    // create renderer view
+    $view = new RendererView($model);
 } catch (InvalidArgumentException $error) {
     // create error rendering model
     $model = new ErrorModel("templates/error.php", $error->getMessage());
+    // create error rendering view
+    $view = new ErrorView($model);
 }
-
-// create renderer view
-$view = new RendererView($model);
 
 // render SVG
 echo $view->output();
