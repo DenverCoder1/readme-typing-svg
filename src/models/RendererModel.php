0d4541eed5e5f70ea38dbdd43e95d081c413900b
Jonah Lawrence <jonah@freshidea.com>
2021-04-20 04:24:59 +0300

    Simplified validation with default value array

--- src/models/RendererModel.php
+++ src/models/RendererModel.php
@@ -2,31 +2,47 @@
 
 class RendererModel
 {
-    // @var array<string> $lines - text to display
+    /** @var array<string> $lines text to display */
     public $lines;
-    // @var string $font - font family
+
+    /** @var string $font font family */
     public $font;
-    // @var string $color - font color
+
+    /** @var string $color font color */
     public $color;
-    // @var int $size - font size
+
+    /** @var int $size font size */
     public $size;
-    // @var bool $center - whether or not to center text
+
+    /** @var bool $center whether or not to center text */
     public $center;
-    // @var int $width - card width (px)
+
+    /** @var int $width SVG width (px) */
     public $width;
-    // @var int $height - card height (px)
+
+    /** @var int $height SVG height (px) */
     public $height;
+
+    /** @var array<string, string> $DEFAULTS */
+    private $DEFAULTS = array(
+        "font" => "JetBrains Mono",
+        "color" => "#36BCF7",
+        "size" => "20",
+        "center" => "false",
+        "width" => "400",
+        "height" => "50",
+    );
 
     public function __construct()
     {
-        $this->lines = $this->validateLines($_REQUEST["lines"]);
-        $this->font = $this->validateFont($_REQUEST["font"], "JetBrains Mono");
-        $this->color = $this->validateColor($_REQUEST["color"], "#36BCF7");
-        $this->size = $this->validateNumber($_REQUEST["size"], 20, "Font size");
-        $this->center = $this->validateCenter($_REQUEST["center"], false);
-        $this->width = $this->validateNumber($_REQUEST["size"], 400, "Width");
-        $this->height = $this->validateNumber($_REQUEST["size"], 50, "Font 20size");
-        $this->template = "tpl/template.php";
+        $this->lines = $this->checkLines($_REQUEST["lines"]);
+        $this->font = $this->checkFont($_REQUEST["font"] ?? $this->DEFAULTS["font"]);
+        $this->color = $this->checkColor($_REQUEST["color"] ?? $this->DEFAULTS["color"]);
+        $this->size = $this->checkNumber($_REQUEST["size"] ?? $this->DEFAULTS["size"], "Font size");
+        $this->center = $this->checkCenter($_REQUEST["center"] ?? $this->DEFAULTS["center"]);
+        $this->width = $this->checkNumber($_REQUEST["width"] ?? $this->DEFAULTS["width"], "Width");
+        $this->height = $this->checkNumber($_REQUEST["height"] ?? $this->DEFAULTS["height"], "Height");
+        $this->template = "templates/svg.php";
     }
 
     /**
@@ -35,7 +51,7 @@ class RendererModel
      * @param string|NULL $lines - semicolon separated lines parameter
      * @return array<string>
      */
-    private function validateLines($lines)
+    private function checkLines($lines)
     {
         if (!isset($lines)) {
             throw new InvalidArgumentException("Lines parameter must be set.");
@@ -50,15 +66,11 @@ class RendererModel
     /**
      * Validate font family and return valid string
      *
-     * @param string|NULL $font - font name parameter
-     * @param string $default - default font
+     * @param string $font - font name parameter
      * @return string
      */
-    private function validateFont($font, $default)
+    private function checkFont($font)
     {
-        if (!isset($font)) {
-            return $default;
-        }
         // return escaped font name
         return (string) preg_replace("/[^0-9A-Za-z+'\-()!&*_ ]/", "", $font);
     }
@@ -67,18 +79,14 @@ class RendererModel
      * Validate font color and return valid string
      *
      * @param string|NULL $color - color parameter
-     * @param string $default - default color
      * @return string
      */
-    private function validateColor($color, $default)
+    private function checkColor($color)
     {
-        if (!isset($color)) {
-            return $default;
-        }
         $escaped = (string) preg_replace("/[^0-9A-Fa-f]/", "", $color);
         // if color is not a valid length, use the default
         if (!in_array(strlen($escaped), [3, 4, 6, 8])) {
-            return $default;
+            return $this->DEFAULTS["color"];
         }
         // return escaped color
         return "#" . $escaped;
@@ -88,15 +96,11 @@ class RendererModel
      * Validate numeric parameter and return valid integer
      *
      * @param string|NULL $num - parameter to validate
-     * @param int $default - default value in case parameter is invalid
      * @param string $field - field name for displaying in case of error
      * @return int
      */
-    private function validateNumber($num, $default, $field)
+    private function checkNumber($num, $field)
     {
-        if (!isset($num)) {
-            return $default;
-        }
         $digits = intval(preg_replace("/[^0-9\-]/", "", $num));
         if ($digits <= 0) {
             throw new InvalidArgumentException("$field must be a positive number.");
@@ -108,11 +112,10 @@ class RendererModel
      * Validate center alignment and return boolean
      *
      * @param string|NULL $center - center parameter
-     * @param boolean $default - default value
      * @return boolean
      */
-    private function validateCenter($center, $default)
+    private function checkCenter($center)
     {
-        return isset($center) ? ($center == "true") : $default;
+        return isset($center) ? ($center == "true") : $this->DEFAULTS["center"];
     }
 }
