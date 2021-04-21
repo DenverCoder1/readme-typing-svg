82e06a9b71c2a74ce9e9a841c57a96dce14aea97
Jonah Lawrence <jonah@freshidea.com>
2021-04-21 20:38:53 +0300

    Simplified escaping of font names

--- src/models/RendererModel.php
+++ src/models/RendererModel.php
@@ -81,7 +81,7 @@ class RendererModel
     private function checkFont($font)
     {
         // return escaped font name
-        return preg_replace("/[^0-9A-Za-z+'\-()!&*_ ]/", "", $font);
+        return preg_replace("/[^0-9A-Za-z\- ]/", "", $font);
     }
 
     /**
