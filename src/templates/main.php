00cfbded0cdb23d9bf5796641f68838b4ba41d15
Jonah Lawrence <jonah@freshidea.com>
2021-04-21 20:42:08 +0300

    Added quotes around font styling
    Also simplified width and height output

--- src/templates/main.php
+++ src/templates/main.php
@@ -1,8 +1,8 @@
 <!-- https://github.com/DenverCoder1/readme-typing-svg/ -->
 <svg xmlns='http://www.w3.org/2000/svg'
 	xmlns:xlink='http://www.w3.org/1999/xlink'
-	viewBox='0 0 <?php echo "{$width} {$height}" ?>'
-	width='<?php echo "{$width}" ?>px' height='<?php echo "{$height}" ?>px'>
+	viewBox='0 0 <?php echo "$width $height" ?>'
+	width='<?php echo $width ?>px' height='<?php echo $height ?>px'>
 
 	<style>
 		@import url('https://fonts.googleapis.com/css2?family=<?php echo str_replace(" ", "+", $font) ?>')
@@ -14,7 +14,7 @@
         <animate id='d<?php echo $i ?>' attributeName='d' begin='<?php echo ($i == 0 ? "0s;" : "") . $previousId ?>.end' dur='5s'
             values='m0,<?php echo $height / 2 ?> h0 ; m0,<?php echo $height / 2 ?> h<?php echo $width ?> ; m0,<?php echo $height / 2 ?> h0' keyTimes='0 ; 0.8 ; 1' />
     </path>
-    <text font-family='<?php echo $font ?>, monospace' fill='<?php echo $color ?>' font-size='<?php echo $size ?>'
+    <text font-family='"<?php echo $font ?>", monospace' fill='<?php echo $color ?>' font-size='<?php echo $size ?>'
 <?php if ($center): ?>
         x='50%' dominant-baseline='middle' text-anchor='middle'>
 <?php else: ?>
