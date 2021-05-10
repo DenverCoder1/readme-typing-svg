066304cf9685295a650d33f9ae4f97338fde8a1a
Jonah Lawrence <jonah@freshidea.com>
2021-05-11 01:07:09 +0300

    Update site buttons with buttons.github.io

--- src/demo/index.php
+++ src/demo/index.php
@@ -28,22 +28,20 @@
 <body <?php echo $_COOKIE["darkmode"] == "on" ? 'data-theme="dark"' : ""; ?>>
     <h1>⌨️ Readme Typing SVG</h1>
 
+    <!-- GitHub badges/links section -->
     <div class="github">
         <!-- GitHub Sponsors -->
-        <iframe src="https://github.com/sponsors/DenverCoder1/button" title="Sponsor DenverCoder1" height="32"
-            width="116" style="border: 0; margin-left: 12px;"></iframe>
-        <!-- GitHub Logo -->
-        <a href="https://git.io/typing-svg" title="GitHub" style="text-decoration: none;">
-            <svg stroke="#0000" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="32px" width="32px"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM277.3 415.7c-8.4 1.5-11.5-3.7-11.5-8 0-5.4.2-33 .2-55.3 0-15.6-5.2-25.5-11.3-30.7 37-4.1 76-9.2 76-73.1 0-18.2-6.5-27.3-17.1-39 1.7-4.3 7.4-22-1.7-45-13.9-4.3-45.7 17.9-45.7 17.9-13.2-3.7-27.5-5.6-41.6-5.6-14.1 0-28.4 1.9-41.6 5.6 0 0-31.8-22.2-45.7-17.9-9.1 22.9-3.5 40.6-1.7 45-10.6 11.7-15.6 20.8-15.6 39 0 63.6 37.3 69 74.3 73.1-4.8 4.3-9.1 11.7-10.6 22.3-9.5 4.3-33.8 11.7-48.3-13.9-9.1-15.8-25.5-17.1-25.5-17.1-16.2-.2-1.1 10.2-1.1 10.2 10.8 5 18.4 24.2 18.4 24.2 9.7 29.7 56.1 19.7 56.1 19.7 0 13.9.2 36.5.2 40.6 0 4.3-3 9.5-11.5 8-66-22.1-112.2-84.9-112.2-158.3 0-91.8 70.2-161.5 162-161.5S388 165.6 388 257.4c.1 73.4-44.7 136.3-110.7 158.3zm-98.1-61.1c-1.9.4-3.7-.4-3.9-1.7-.2-1.5 1.1-2.8 3-3.2 1.9-.2 3.7.6 3.9 1.9.3 1.3-1 2.6-3 3zm-9.5-.9c0 1.3-1.5 2.4-3.5 2.4-2.2.2-3.7-.9-3.7-2.4 0-1.3 1.5-2.4 3.5-2.4 1.9-.2 3.7.9 3.7 2.4zm-13.7-1.1c-.4 1.3-2.4 1.9-4.1 1.3-1.9-.4-3.2-1.9-2.8-3.2.4-1.3 2.4-1.9 4.1-1.5 2 .6 3.3 2.1 2.8 3.4zm-12.3-5.4c-.9 1.1-2.8.9-4.3-.6-1.5-1.3-1.9-3.2-.9-4.1.9-1.1 2.8-.9 4.3.6 1.3 1.3 1.8 3.3.9 4.1zm-9.1-9.1c-.9.6-2.6 0-3.7-1.5s-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3 1.1 1.5 1.1 3.3 0 4.1zm-6.5-9.7c-.9.9-2.4.4-3.5-.6-1.1-1.3-1.3-2.8-.4-3.5.9-.9 2.4-.4 3.5.6 1.1 1.3 1.3 2.8.4 3.5zm-6.7-7.4c-.4.9-1.7 1.1-2.8.4-1.3-.6-1.9-1.7-1.5-2.6.4-.6 1.5-.9 2.8-.4 1.3.7 1.9 1.8 1.5 2.6z">
-                </path>
-            </svg></a>
+        <a class="github-button" href="https://github.com/sponsors/denvercoder1"
+            data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-heart"
+            data-size="large" aria-label="Sponsor @denvercoder1 on GitHub">Sponsor</a>
+        <!-- View on GitHub -->
+        <a class="github-button" href="https://github.com/denvercoder1/unicode-formatter"
+            data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large"
+            aria-label="View denvercoder1/unicode-formatter on GitHub">View on GitHub</a>
         <!-- GitHub Star -->
-        <iframe
-            src="https://ghbtns.com/github-btn.html?user=DenverCoder1&repo=readme-typing-svg&type=star&count=true&size=large"
-            width="140" height="30" title="GitHub stars" style="border: 0;"></iframe>
+        <a class="github-button" href="https://github.com/denvercoder1/unicode-formatter"
+            data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star"
+            data-size="large" data-show-count="true" aria-label="Star denvercoder1/unicode-formatter on GitHub">Star</a>
     </div>
 
     <div class="container">
